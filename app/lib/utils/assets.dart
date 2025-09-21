46157c19f86cc8e5aa58de5f240d86f4425bd329
Anand Suthar <anandsuthar956@gmail.com>
2025-09-21 23:36:03 +0530

    feat: add assets and responsive utils

---
+++ app/lib/utils/assets.dart
@@ -0,0 +1,6 @@
+class Assets {
+  static const String patientIcon = 'assets/icons/patient.png';
+
+  // Placeholder for future assets
+  static const String logoIcon = 'assets/icons/patient.png';
+}
